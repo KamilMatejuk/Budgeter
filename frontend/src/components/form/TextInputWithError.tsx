3c1288dbbf75a35e3d06a3c115f3d9d17c8f2208
KamilMatejuk <kamilmatejuk@gmail.com>
2025-07-01 15:15:23 +0200

    add list of objects into formik

--- frontend/src/components/form/TextInputWithError.tsx
+++ frontend/src/components/form/TextInputWithError.tsx
@@ -2,9 +2,41 @@ import React from "react";
 import { FormikProps } from "formik";
 import { twMerge } from "tailwind-merge";
 
-export interface TextInputWithErrProps<T> extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface TextInputWithErrProps<T>
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   formik: FormikProps<T>;
-  formikName: keyof T;
+  formikName: keyof T | string;
+}
+
+function getFromFormik<T>(
+  formik: FormikProps<T>,
+  formikObject: keyof FormikProps<T>,
+  formikName: keyof T | string
+) {
+  if ((formikName as string).includes("[")) {
+    const segments = (formikName as string).replace(/\[(\d+)\]/g, ".$1").split(".");
+    let value = formik[formikObject];
+    for (const segment of segments) {
+      if (value === undefined) return undefined;
+      value = typeof value === "object" ? value[segment] : undefined;
+    }
+    return value;
+  }
+  return formik[formikObject][formikName] as string;
+}
+
+export function getError<T>(
+  formik: FormikProps<T>,
+  formikName: keyof T | string
+) {
+  return getFromFormik(formik, "errors", formikName);
+}
+
+export function getTouched<T>(
+  formik: FormikProps<T>,
+  formikName: keyof T | string
+) {
+  return getFromFormik(formik, "touched", formikName);
 }
 
 export default function TextInputWithError<T>({
@@ -12,8 +44,8 @@ export default function TextInputWithError<T>({
   formikName,
   ...props
 }: TextInputWithErrProps<T>) {
-  const error = formik.errors[formikName] as string;
-  const touched = formik.touched[formikName];
+  const error = getError(formik, formikName);
+  const touched = getTouched(formik, formikName);
 
   return (
     <div>
@@ -26,7 +58,7 @@ export default function TextInputWithError<T>({
           props.className || ""
         )}
         {...formik.getFieldProps(formikName as string)}
-        />
+      />
       {error && touched && (
         <p className="text-red-500 text-sm text-center">{error}</p>
       )}

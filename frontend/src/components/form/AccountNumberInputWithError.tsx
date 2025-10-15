36e4b001905738397ec26927f14e10b3f8069834
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-15 15:42:20 +0200

    differentiate between card and account number inputs

---
+++ frontend/src/components/form/AccountNumberInputWithError.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { z } from "zod";
+import { ERROR } from "@/const/message";
+import TextInputWithError, { getValue, TextInputWithErrorProps } from "./TextInputWithError";
+
+
+export const requiredAccountNumber = z.preprocess(
+  (val) => (val as string).replace(/\s+/g, ''),
+  z.string({ required_error: ERROR.requiredError }).superRefine((val, ctx) => {
+    if (val.startsWith("PL") && val.length !== 28)
+      ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Account number with PL must be 28 characters long" });
+    if (!val.startsWith("PL") && val.length !== 26)
+      ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Account number without PL must be 26 characters long" });
+  })
+);
+
+function format(value: string) {
+  // remove non-numeric characters except PL
+  value = value.toUpperCase().replace(/[^0-9PL]/g, "").trim();
+  // insert space every 4 characters
+  if (!value.startsWith("PL")) {
+    return `PL${value}`.replace(/(.{4})/g, "$1 ").trim().slice(2);
+  }
+  return value.replace(/(.{4})/g, "$1 ").trim();
+}
+
+export default function AccountNumberInputWithError<T>({
+  formik,
+  formikName,
+  label,
+}: TextInputWithErrorProps<T>) {
+  const value = getValue(formik, formikName);
+
+  function set(newValue: string) {
+    newValue = format(newValue);
+    if (newValue.length > value.length) {
+      // maximum length 26/28 characters + spaces between
+      if (/^[0-9]/.test(newValue) && value.length === 26 + 6) return;
+      if (!/^[0-9]/.test(newValue) && value.length === 28 + 6) return;
+    }
+    formik.setFieldValue(formikName as string, newValue);
+  }
+
+  return (
+    <TextInputWithError
+      formik={formik}
+      formikName={formikName}
+      label={label}
+      inputMode="decimal"
+      value={value}
+      onChange={(e) => {
+        set(e.target.value);
+        // fix cursor position (don't move to the end)
+        const cursorPos = e.target.selectionStart || e.target.value.length;
+        requestAnimationFrame(() => e.target.setSelectionRange(cursorPos, cursorPos));
+      }}
+      onBlur={() => set(value)}
+    />
+  );
+}
--- frontend/src/components/form/CardNumberInputWithError.tsx
+++ frontend/src/components/form/CardNumberInputWithError.tsx
@@ -7,12 +7,14 @@ import TextInputWithError, { getValue, TextInputWithErrorProps } from "./TextInp
 export const requiredCardNumber = z.preprocess(
   (val) => (val as string).replace(/\s+/g, ''),
   z.string({ required_error: ERROR.requiredError })
-    .refine((val) => val.length === 16 || val.length === 28, "Card number must be either 16 or 28 characters long")
+    .refine((val) => val.length === 16, "Card number must be 16 characters long")
 );
 
 function format(value: string) {
-  // remove non-numeric characters except X
-  value = value.toUpperCase().replace(/[^0-9A-Z]/g, "").trim();
+  // remove non-numeric characters
+  value = value.toUpperCase().replace(/[^0-9]/g, "").trim();
+  // pad with X
+  if (value.length > 4) value = value.slice(0, 4) + "XXXXXXXX" + value.slice(4);
   // insert space every 4 characters
   return value.replace(/(.{4})/g, "$1 ").trim();
 }
@@ -26,8 +28,8 @@ export default function CardNumberInputWithError<T>({
 
   function set(newValue: string) {
     newValue = format(newValue);
-    // maximum 28 charasters + spaces between
-    if (value.length === (4 * 7 + 6) && newValue.length > value.length) return;
+    // maximum 16 characters + spaces between
+    if (newValue.length > value.length && value.length === 16 + 3) return;
     formik.setFieldValue(formikName as string, newValue);
   }
 
@@ -41,7 +43,8 @@ export default function CardNumberInputWithError<T>({
       onChange={(e) => {
         set(e.target.value);
         // fix cursor position (don't move to the end)
-        const cursorPos = e.target.selectionStart || e.target.value.length;
+        let cursorPos = e.target.selectionStart || e.target.value.length;
+        if (cursorPos > 4 && cursorPos < 14) cursorPos += 11;
         requestAnimationFrame(() => e.target.setSelectionRange(cursorPos, cursorPos));
       }}
       onBlur={() => set(value)}

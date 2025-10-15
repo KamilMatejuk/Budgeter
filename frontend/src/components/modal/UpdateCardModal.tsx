4764c78ac3e23547eb72a4644005c02c3c5f7475
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-15 11:47:17 +0200

    add card modal

---
+++ frontend/src/components/modal/UpdateCardModal.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Modal from "./Modal";
+import { submit, UpdateModalProps } from "./UpdateModal";
+import { z } from "zod";
+import { ERROR } from "@/const/message";
+import { Card, CardWithId } from "@/types/backend";
+import { useFormik } from "formik";
+import { withZodSchema } from "formik-validator-zod";
+import TextInputWithError, { requiredText } from "../form/TextInputWithError";
+import { Currency } from "@/types/enum";
+import AmountInputWithError, { requiredAmount } from "../form/AmountInputWithError";
+import ChoiceInputWithError from "../form/ChoiceInputWithError";
+import CardNumberInputWithError, { requiredCardNumber } from "../form/CardNumberInputWithError";
+
+enum Type {
+    DEBIT = "DEBIT",
+    CREDIT = "CREDIT",
+}
+
+const FormSchema = z.object({
+    name: requiredText,
+    number: requiredCardNumber,
+    value: requiredAmount,
+    currency: z.nativeEnum(Currency, { required_error: ERROR.requiredError }),
+    credit: z.nativeEnum(Type, { required_error: ERROR.requiredError }),
+});
+type FormSchemaType = z.infer<typeof FormSchema>;
+type SubmitFormSchemaType = Omit<FormSchemaType, "credit"> & { credit: boolean };
+
+
+
+export default function UpdateCardModal({ url, item, open, onClose }: UpdateModalProps<Card>) {
+    const formik = useFormik<FormSchemaType>({
+        initialValues: {
+            name: item.name || "",
+            value: item.value || 0,
+            currency: item.currency as Currency || Currency.PLN,
+            number: item.number || "",
+            credit: item.credit ? Type.CREDIT : Type.DEBIT,
+        },
+        onSubmit: async (values) => {
+            if (await submit<SubmitFormSchemaType, CardWithId>(url, {...values, credit: values.credit == Type.CREDIT}, item?._id)) {
+                onClose();
+            }
+        },
+        validate: withZodSchema(FormSchema),
+    });
+
+    return (
+        <Modal open={open} onClose={onClose} cancellable onSave={formik.submitForm}>
+            <TextInputWithError formik={formik} formikName="name" label="Name" />
+            <CardNumberInputWithError formik={formik} formikName="number" label="Number" />
+            <AmountInputWithError formik={formik} formikName="value" label="Value" />
+            <ChoiceInputWithError formik={formik} formikName="currency" optionsEnum={Currency} label="Currency" />
+            <ChoiceInputWithError formik={formik} formikName="credit" optionsEnum={Type} label="Type" />
+        </Modal>
+    );
+}
--- frontend/src/components/modal/UpdateModal.tsx
+++ frontend/src/components/modal/UpdateModal.tsx
@@ -3,8 +3,9 @@ import { ModalProps } from "./Modal";
 import { Item, ItemID } from "../table/Table";
 import UpdateSourceModal from "./UpdateSourceModal";
 import { patch, post } from "@/app/api/fetch";
-import { Cash, Source } from "@/types/backend";
+import { Card, Cash, Source } from "@/types/backend";
 import UpdateCashModal from "./UpdateCashModal";
+import UpdateCardModal from "./UpdateCardModal";
 
 
 export interface UpdateModalProps<T extends Item> extends ModalProps {
@@ -24,4 +25,5 @@ export async function submit<FST, TID extends ItemID>(url: string, values: FST, 
 export default function UpdateModal<T extends Item>({ url, item, open, onClose }: UpdateModalProps<T>) {
     if (url === "/api/source") return <UpdateSourceModal open={open} onClose={onClose} item={item as unknown as Source} url={url} />;
     if (url === "/api/products/cash") return <UpdateCashModal open={open} onClose={onClose} item={item as unknown as Cash} url={url} />;
+    if (url === "/api/products/card") return <UpdateCardModal open={open} onClose={onClose} item={item as unknown as Card} url={url} />;
 }

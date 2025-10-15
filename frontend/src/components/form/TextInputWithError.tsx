8055e0aa1f883dbf6c833a1cd801aabb5c691bc1
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-15 11:45:55 +0200

    fix props overriding by formik

--- frontend/src/components/form/TextInputWithError.tsx
+++ frontend/src/components/form/TextInputWithError.tsx
@@ -50,13 +50,13 @@ export default function TextInputWithError<T>({ formik, formikName, label, ...pr
       {label && <label className="">{label}</label>}
       <input
         type="text"
+        {...formik.getFieldProps(formikName as string)}
         {...props}
         className={twMerge(
           "border border-gray-300 px-4 py-2 rounded-md w-full text-center",
           error && touched ? "border-red-200 bg-red-500/10" : "",
           props.className || ""
         )}
-        {...formik.getFieldProps(formikName as string)}
       />
       {error && touched && (
         <p className="text-red-500 text-sm text-center">{error}</p>

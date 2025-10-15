5292392385e99c140083fb307b84c16aed30d48d
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-15 18:49:31 +0200

    add stock account modal

--- frontend/src/components/modal/UpdateModal.tsx
+++ frontend/src/components/modal/UpdateModal.tsx
@@ -3,11 +3,12 @@ import { ModalProps } from "./Modal";
 import { Item, ItemID } from "../table/Table";
 import UpdateSourceModal from "./UpdateSourceModal";
 import { patch, post } from "@/app/api/fetch";
-import { Card, Cash, PersonalAccount, SavingsAccount, Source } from "@/types/backend";
+import { Card, Cash, PersonalAccount, SavingsAccount, Source, StockAccount } from "@/types/backend";
 import UpdateCashModal from "./UpdateCashModal";
 import UpdateCardModal from "./UpdateCardModal";
 import UpdatePersonalAccountModal from "./UpdatePersonalAccountModal";
 import UpdateSavingsAccountModal from "./UpdateSavingsAccountModal";
+import UpdateStockAccountModal from "./UpdateStockAccountModal";
 
 
 export interface UpdateModalProps<T extends Item> extends ModalProps {
@@ -30,4 +31,5 @@ export default function UpdateModal<T extends Item>({ url, item, open, onClose }
     if (url === "/api/products/card") return <UpdateCardModal open={open} onClose={onClose} item={item as unknown as Card} url={url} />;
     if (url === "/api/products/personal_account") return <UpdatePersonalAccountModal open={open} onClose={onClose} item={item as unknown as PersonalAccount} url={url} />;
     if (url === "/api/products/savings_account") return <UpdateSavingsAccountModal open={open} onClose={onClose} item={item as unknown as SavingsAccount} url={url} />;
+    if (url === "/api/products/stock_account") return <UpdateStockAccountModal open={open} onClose={onClose} item={item as unknown as StockAccount} url={url} />;
 }
---
+++ frontend/src/components/modal/UpdateStockAccountModal.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Modal from "./Modal";
+import { submit, UpdateModalProps } from "./UpdateModal";
+import { z } from "zod";
+import { ERROR } from "@/const/message";
+import { StockAccount, StockAccountWithId } from "@/types/backend";
+import { useFormik } from "formik";
+import { withZodSchema } from "formik-validator-zod";
+import TextInputWithError, { requiredText } from "../form/TextInputWithError";
+import { Currency } from "@/types/enum";
+import AmountInputWithError, { requiredNonNegativeAmount, requiredPositiveAmount } from "../form/AmountInputWithError";
+import ChoiceInputWithError from "../form/ChoiceInputWithError";
+import AccountNumberInputWithError, { requiredAccountNumber } from "../form/AccountNumberInputWithError";
+
+
+const FormSchema = z.object({
+    name: requiredText,
+    number: requiredAccountNumber,
+    value: requiredNonNegativeAmount,
+    currency: z.nativeEnum(Currency, { required_error: ERROR.requiredError }),
+    yearly_interest: requiredPositiveAmount,
+});
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+
+export default function UpdateStockAccountModal({ url, item, open, onClose }: UpdateModalProps<StockAccount>) {
+    const formik = useFormik<FormSchemaType>({
+        initialValues: {
+            name: item.name || "",
+            number: item.number || "",
+            value: item.value || 0,
+            currency: item.currency as Currency || Currency.PLN,
+            yearly_interest: item.yearly_interest || 0,
+        },
+        onSubmit: async (values) => { if (await submit<FormSchemaType, StockAccountWithId>(url, values, item?._id)) onClose() },
+        validate: withZodSchema(FormSchema),
+    });
+
+return (
+    <Modal open={open} onClose={onClose} cancellable onSave={formik.submitForm}>
+        <TextInputWithError formik={formik} formikName="name" label="Name" />
+        <AccountNumberInputWithError formik={formik} formikName="number" label="Number" />
+        <AmountInputWithError formik={formik} formikName="value" label="Value" />
+        <ChoiceInputWithError formik={formik} formikName="currency" optionsEnum={Currency} label="Currency" />
+        <AmountInputWithError formik={formik} formikName="yearly_interest" label="Yearly Interest" />
+    </Modal>
+);
+}

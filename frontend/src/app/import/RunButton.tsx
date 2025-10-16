39f6d69759ba27778332eaf90154dd43c2cee2bc
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-16 14:40:57 +0200

    fix

--- frontend/src/app/import/RunButton.tsx
+++ frontend/src/app/import/RunButton.tsx
@@ -98,15 +98,3 @@ export default function RunButton() {
     </>
   );
 }
-
-
-// TODO
-// * move accounts to products
-// * load positive and negative values correctly
-// * distinguish moving between accounts and normal transactions
-
-
-// top bar - page title
-// left sidebar - logo, current balance, menu (dashboards, import, settings, predictions, help)
-// https://cdn.dribbble.com/userupload/4307413/file/still-328dbb0cf5ae384f442c56491df7505d.png
-// settings - black title, grey descriptions, table for each type - each edit is a new screen

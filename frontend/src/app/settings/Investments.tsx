4bd0b583a048d239cc0d90cc24d1709f714e9a7c
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-25 22:14:34 +0200

    reorder

--- frontend/src/app/settings/Investments.tsx
+++ frontend/src/app/settings/Investments.tsx
@@ -4,20 +4,12 @@ import Table from "@/components/table/Table";
 import ErrorToast from "@/components/toast/ErrorToast";
 
 export default async function Investments() {
-  const { response: savings, error: savingsAccountError } = await get<SavingsAccountWithId[]>("/api/products/savings_account", ["savings_account"]);
   const { response: stock, error: stockAccountError } = await get<StockAccountWithId[]>("/api/products/stock_account", ["stock_account"]);
   const { response: capital, error: capitalInvestmentError } = await get<CapitalInvestmentWithId[]>("/api/products/capital_investment", ["capital_investment"]);
+  const { response: savings, error: savingsAccountError } = await get<SavingsAccountWithId[]>("/api/products/savings_account", ["savings_account"]);
 
   return (
     <>
-      {savingsAccountError
-        ? <ErrorToast message={`Could not download saving accounts: ${savingsAccountError.message}`} />
-        : <Table<SavingsAccount, SavingsAccountWithId>
-          url="/api/products/savings_account"
-          tag="savings_account"
-          newText="savings account"
-          data={savings}
-          columns={["name", "value", "currency", "number", "interest", "capitalization"]} />}
       {stockAccountError
         ? <ErrorToast message={`Could not download stock accounts: ${stockAccountError.message}`} />
         : <Table<StockAccount, StockAccountWithId>
@@ -34,6 +26,14 @@ export default async function Investments() {
           newText="capital investment"
           data={capital}
           columns={["name", "value", "currency", "number", "capitalization", "startDate", "endDate"]} />}
+      {savingsAccountError
+        ? <ErrorToast message={`Could not download saving accounts: ${savingsAccountError.message}`} />
+        : <Table<SavingsAccount, SavingsAccountWithId>
+          url="/api/products/savings_account"
+          tag="savings_account"
+          newText="savings account"
+          data={savings}
+          columns={["name", "value", "currency", "number", "interest", "capitalization"]} />}
     </>
   );
 }

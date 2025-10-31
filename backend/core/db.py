d51ba7a292961477f5c5be2b0b9238ab21998093
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-31 14:24:21 +0100

    fix old source

--- backend/core/db.py
+++ backend/core/db.py
@@ -27,7 +27,6 @@ async def create_indexes(db: AsyncIOMotorDatabase):
 
 async def clear_db(db: AsyncIOMotorDatabase):
     # await db["tags"].delete_many({})
-    # await db["sources"].delete_many({})
     await db["transactions"].delete_many({})
     await db["card_monthly_history"].delete_many({})
     # await db.drop_collection("transactions")

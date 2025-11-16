6ff9d935cf00a2ff7b314461621071c9668742b2
KamilMatejuk <kamilmatejuk@gmail.com>
2025-11-16 23:10:08 +0100

    parse string and int

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,5 +1,13 @@
 class Value:
     """Utility class to handle float values with two decimal places."""
+    
+    @staticmethod
+    def parse(v: str | int | float) -> float:
+        return Value._double_precision(float(v))
+
+    @staticmethod
+    def parse_negate(v: str | int | float) -> float:
+        return Value._double_precision(-float(v))
 
     @staticmethod
     def _double_precision(v: float) -> float:

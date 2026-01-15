4619789829dedd6b05d7135c06025ee7d9cc4115
KamilMatejuk <kamilmatejuk@gmail.com>
2026-01-15 14:54:45 +0100

    remove obsolete

--- frontend/src/components/form/TagsInputWithError.tsx
+++ frontend/src/components/form/TagsInputWithError.tsx
@@ -5,18 +5,6 @@ import { useOrganisation, useTags } from "@/app/api/query";
 import SearchableTextInputWithError from "./SearchableTextInputWithError";
 
 
-const classes = {
-  container: "relative",
-  select: "border border-gray-300 px-4 py-2 rounded-md w-full text-center appearance-none",
-  selectError: "border-red-200 bg-red-500/10",
-  icon: "absolute right-3 top-1/2 -translate-y-1/2 transition-transform duration-300 pointer-events-none",
-  dropdown: "absolute left-0 right-0 mt-1 max-h-60 overflow-auto bg-white border rounded shadow z-10",
-  option: "px-1 py-1 hover:bg-gray-100 cursor-pointer",
-  optionHighlighted: "bg-gray-100",
-  selected: "flex flex-wrap max-w-96",
-  selectedTag: "flex items-center gap-1 mb-1 mr-1",
-};
-
 interface TagsInputWithErrorProps<T> extends SingleInputWithErrorProps<T> {
   organisationName?: string;
 }

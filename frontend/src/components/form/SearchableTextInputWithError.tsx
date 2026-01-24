4d487873f01923d1e3a7d3e1b0f970ae3b81ae6c
KamilMatejuk <kamilmatejuk@gmail.com>
2026-01-24 02:01:35 +0100

    add clearing selection in searchfield

--- frontend/src/components/form/SearchableTextInputWithError.tsx
+++ frontend/src/components/form/SearchableTextInputWithError.tsx
@@ -8,7 +8,8 @@ const classes = {
   container: "relative",
   select: "border border-gray-300 px-4 py-2 rounded-md w-full text-center appearance-none",
   selectError: "border-red-200 bg-red-500/10",
-  icon: "absolute right-3 top-1/2 -translate-y-1/2 transition-transform duration-300 pointer-events-none",
+  iconExpand: "absolute right-3 top-1/2 -translate-y-1/2 transition-transform duration-300 pointer-events-none",
+  iconClear: "absolute right-9 top-1/2 -translate-y-1/2 cursor-pointer",
   dropdown: "absolute left-0 right-0 mt-0 max-h-60 overflow-auto bg-white border rounded shadow z-10",
   option: "px-1 py-1 hover:bg-gray-100 cursor-pointer",
   optionLabel: "hover:bg-inherit text-subtext text-sm",
@@ -136,11 +137,17 @@ export default function SearchableTextInputWithError<T>({
             ))}
           </div>
         )}
-        {/* icon */}
-        <MdExpandMore
-          className={twMerge(classes.icon, open && "rotate-180")}
-          size={20}
-        />
+        {/* icons */}
+        <MdExpandMore className={twMerge(classes.iconExpand, open && "rotate-180")} size={20} />
+        <MdClose className={classes.iconClear} size={15} onClick={() => {
+          if (singleSelect) {
+            formik.setFieldValue(formikName as string, "", true);
+          } else {
+            setSelectedIds([]);
+            formik.setFieldValue(formikName as string, [], true);
+          }
+          setSearch("");
+        }} />
         {/* rendered selected option in single select mode */}
         {singleSelect && value && typeof value === "string" &&
           <div className={classes.singleSelectedTag} onClick={() => formik.setFieldValue(formikName as string, "", true)}>

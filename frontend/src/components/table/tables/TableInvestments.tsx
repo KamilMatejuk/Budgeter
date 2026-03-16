ffab31f1635bc8278d0713a656d77fe494b1ed40
Kamil Matejuk <kamilmatejuk@gmail.com>
2026-03-16 10:34:25 +0100

    estimated return from capital investment

--- frontend/src/components/table/tables/TableInvestments.tsx
+++ frontend/src/components/table/tables/TableInvestments.tsx
@@ -1,31 +1,46 @@
 'use client';
 
-import { CapitalInvestmentWithId } from "@/types/backend";
+import { CapitalInvestmentRichWithId } from "@/types/backend";
 import Table from "@/components/table/Table";
 import { ColumnDef } from "@tanstack/react-table";
-import { defineCellValue } from "../cells/CellValue";
+import CellValue, { defineCellValue } from "../cells/CellValue";
 import UpdateCapitalInvestmentModal from "@/components/modal/update/UpdateCapitalInvestmentModal";
 import DeleteByIdModal from "@/components/modal/delete/DeleteByIdModal";
 import { MdDelete, MdEdit } from "react-icons/md";
 import CellDate from "../cells/CellDate";
+import { getMonthsBetweenDates } from "@/const/date";
+import { Currency } from "@/types/enum";
 
 
 interface TableInvestmentsProps {
-  data: CapitalInvestmentWithId[];
+  data: CapitalInvestmentRichWithId[];
 }
 
-const columns: ColumnDef<CapitalInvestmentWithId>[] = [
+const columns: ColumnDef<CapitalInvestmentRichWithId>[] = [
   { accessorKey: "name", header: "Name" },
-  defineCellValue<CapitalInvestmentWithId>(),
-  { accessorKey: "yearly_interest", header: "Yearly Interest", cell: ({ row }) => row.original.yearly_interest.toFixed(1) + "%" },
-  { accessorKey: "capitalization", header: "Capitalization" },
+  defineCellValue<CapitalInvestmentRichWithId>(),
+  { accessorKey: "yearly_interest", header: "Yearly Interest",
+    meta: { align: 'center' },
+    cell: ({ row }) => row.original.yearly_interest.toFixed(1) + "%",
+  },
+  { accessorKey: "estimated_return", header: "Estimated Return",
+    meta: { align: 'center' },
+    cell: ({ row }) => {
+      const length = getMonthsBetweenDates(new Date(row.original.start), new Date(row.original.end))
+      const rate = Math.pow(1 + row.original.yearly_interest / 100, length / 12);
+      const profit = (rate - 1.0) * row.original.value_pln;
+      const taxedProfit = profit * 0.81;
+      return <CellValue value={taxedProfit} colour currency={Currency.PLN} />
+    },
+  },
+  { accessorKey: "capitalization", header: "Capitalization", meta: { align: 'center' } },
   { accessorKey: "start", header: "Start Date", cell: ({ row }) => <CellDate value={row.original.start} showDelta /> },
   { accessorKey: "end", header: "End Date", cell: ({ row }) => <CellDate value={row.original.end} showDelta /> },
 ];
 
 export default function TableInvestments({ data }: TableInvestmentsProps) {
   return (
-    <Table<CapitalInvestmentWithId>
+    <Table<CapitalInvestmentRichWithId>
       url="/api/products/capital_investment"
       tag="capital_investment"
       newText="capital investment"
